60a6e44a066adefbff85f3acfc4185088dcd2d62
Thomas Michaux <michaux@levant.abes.fr>
2023-01-20 12:01:09 +0100

    fr langauge mandatory

--- wikibase_stack/LocalSettings.php
+++ wikibase_stack/LocalSettings.php
@@ -1,3 +1,10 @@
 <?php
 
 $wgServer = false;
+
+// disable language selection
+$wgHiddenPrefs[] = 'language';
+// if you want to disable variants as well
+$wgHiddenPrefs[] = 'variant';
+$wgHiddenPrefs[] = 'noconvertlink';
+$wgLanguageCode = 'fr;
