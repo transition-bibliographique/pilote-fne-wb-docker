b64cf4dc89cd32185072583f5dd6f5b4c332fec9
Aurelien <charot@abes.fr>
2023-02-20 14:33:48 +0100

    Maj indexation SPARQL

--- pilote-fne-wb/scriptsIndexation/indexationSPARQL.ps1
+++ pilote-fne-wb/scriptsIndexation/indexationSPARQL.ps1
@@ -2,6 +2,7 @@
 
 $containerWikibase = "pilote-fne-wikibase"
 $containerWDQS = "pilote-fne-wdqs"
+$containerWDQSUpdater = "pilote-fne-wdqs-updater"
 
 Write-Output "Indexation WDQS / SPARQ"
 
@@ -10,7 +11,11 @@ $current = [System.DateTime]::Now
 docker exec $containerWikibase bash -c 'php extensions/Wikibase/repo/maintenance/dumpRdf.php --server http://${WIKIBASE_HOST} | gzip > MyWiki.ttl.gz'
 docker cp ${containerWikibase}:/var/www/html/MyWiki.ttl.gz .
 docker cp MyWiki.ttl.gz ${containerWDQS}:/wdqs
+docker exec $containerWDQS bash -c 'rm /wdqs/data/*'
+docker stop $containerWDQSUpdater
+docker restart $containerWDQS
 docker exec $containerWDQS bash -c 'curl http://${WDQS_HOST}:${WDQS_PORT}/bigdata/namespace/wdq/sparql --data-urlencode """update=LOAD <file:///wdqs/MyWiki.ttl.gz>;"""'
+docker start $containerWDQSUpdater
 
 $end= [System.DateTime]::Now
 
--- pilote-fne-wb/scriptsIndexation/indexationSPARQL.sh
+++ pilote-fne-wb/scriptsIndexation/indexationSPARQL.sh
@@ -2,6 +2,7 @@
 
 containerWikibase="pilote-fne-wikibase"
 containerWDQS="pilote-fne-wdqs"
+containerWDQSUpdater="pilote-fne-wdqs-updater"
 
 echo "Indexation WDQS / SPARQ"
 
@@ -10,7 +11,11 @@ current=$(date +"%s")
 sudo docker exec $containerWikibase bash -c 'php extensions/Wikibase/repo/maintenance/dumpRdf.php --server http://${WIKIBASE_HOST} | gzip > MyWiki.ttl.gz'
 sudo docker cp ${containerWikibase}:/var/www/html/MyWiki.ttl.gz .
 sudo docker cp MyWiki.ttl.gz ${containerWDQS}:/wdqs
+sudo docker exec $containerWDQS bash -c 'rm /wdqs/data/*'
+sudo docker stop $containerWDQSUpdater
+sudo docker restart $containerWDQS
 sudo docker exec $containerWDQS bash -c 'curl http://${WDQS_HOST}:${WDQS_PORT}/bigdata/namespace/wdq/sparql --data-urlencode """update=LOAD <file:///wdqs/MyWiki.ttl.gz>;"""'
+sudo docker start $containerWDQSUpdater
 
 end=$(date +"%s")
 
